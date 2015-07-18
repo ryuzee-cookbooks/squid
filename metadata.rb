fbe5dc7d6c6eb9bb281e1461d9789e56f7c48cd1
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-07-18 11:48:25 +0900

    fix

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'MIT License'
 description      'Installs/Configures squid'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+%w{redhat centos scientific fedora amazon}.each do |os|
+  supports os
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,20 +6,21 @@
 #
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
+case node['platform']
+when 'centos', 'redhat', 'amazon', 'scientific', 'fedora'
+  package 'squid' do
+    action :upgrade
+  end
 
-package 'squid' do
-  action :upgrade
+  template '/etc/squid/squid.conf' do
+    source 'squid.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '00644'
+  end
+
+  service "squid" do
+    action [:enable, :restart]
+  end
 end
-
-template '/etc/squid/squid.conf' do
-  source 'squid.conf.erb'
-  owner 'root'
-  group 'root'
-  mode '00644'
-end
-
-service "squid" do
-  action [:enable, :restart]
-end
-
 # vim: filetype=ruby.chef
